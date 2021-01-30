f73806de6e7ff46eca228d46239bc65373e02290
Jordan Kelly <profjjk@gmail.com>
2021-01-30 11:29:14 -0800

    remove duplicate and add missing syntax

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,22 +1,20 @@
 INSERT INTO books (book_name, author, finished)
-VALUES  ("The Great Influenza", "John M. Barry", false)
+VALUES  ("The Great Influenza", "John M. Barry", false);
 INSERT INTO books (book_name, author, finished)
-VALUES  ("Sillness is the Key", "Ryan Holiday", true)
+VALUES  ("Sillness is the Key", "Ryan Holiday", true);
 INSERT INTO books (book_name, author, finished)
-VALUES  ("Rise to Globalism", "Stephen E. Ambrose", true)
+VALUES  ("Rise to Globalism", "Stephen E. Ambrose", true);
 INSERT INTO books (book_name, author, finished)
-VALUES  ("It Was ALL A Lie", "Stuart Stevens", true)
+VALUES  ("It Was ALL a Lie", "Stuart Stevens", true);
 INSERT INTO books (book_name, author, finished)
-VALUES  ("The Lean Startup", "Eric Ries", false)
+VALUES  ("The Lean Startup", "Eric Ries", false);
 INSERT INTO books (book_name, author, finished)
-VALUES  ("The Great Influenza", "John M. Barry", false)
+VALUES  ("Grant", "Ron Chernow", true);
 INSERT INTO books (book_name, author, finished)
-VALUES  ("Grant", "Ron Chernow", true)
+VALUES  ("The Guns of August", "Barbara W. Tuchman", true);
 INSERT INTO books (book_name, author, finished)
-VALUES  ("The Guns of August", "Barbara W. Tuchman", true)
+VALUES  ("The Storm Before the Storm", "John M. Barry", true);
 INSERT INTO books (book_name, author, finished)
-VALUES  ("The Storm Before the Storm", "John M. Barry", true)
+VALUES  ("1776", "David McCullough", true);
 INSERT INTO books (book_name, author, finished)
-VALUES  ("1776", "David McCullough", true)
-INSERT INTO books (book_name, author, finished)
-VALUES  ("The War of Art", "Steven Pressfield", false)
+VALUES  ("The War of Art", "Steven Pressfield", false);

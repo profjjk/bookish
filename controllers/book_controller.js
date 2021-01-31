48da9a107b6d7f5e34d9053687484020aa519dea
Jordan Kelly <profjjk@gmail.com>
2021-01-30 22:50:57 -0800

    write some html and css

---
+++ controllers/book_controller.js
@@ -0,0 +1,33 @@
+// DEPENDENCIES
+// ================================================
+const express = require('express');
+const book = require('/../models/book.js')
+
+const router = express.Router();
+
+// ROUTES
+// ================================================
+router.get("/", function(req, res) {
+  book.all(function(data) {
+    const bookObj = {
+      books: data
+    }
+    console.log(bookObj);
+    res.render("index", bookObj);
+  });
+});
+
+router.post("/api/books", function(req, res) {
+  book.insert(req.body.book_info, function(result) {
+    res.json({ id: result.insertId })
+  })
+})
+
+router.put("/api/books/:id", function(req, res) {
+  const id = req.params.id;
+  let boolean = false;
+  
+  book.update(id, )
+})
+
+module.exports = router;

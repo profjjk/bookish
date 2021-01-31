accb281e656a864f9eb0fd4ee910123df1614674
Jordan Kelly <profjjk@gmail.com>
2021-01-30 19:04:09 -0800

    create all, insert, update, delete functions in orm

--- config/orm.js
+++ config/orm.js
@@ -3,15 +3,40 @@
 const connection = require('connection');
 
 const orm = {
-  all: function() {
-
+  all: function(cb) {
+    let queryString = `SELECT * FROM books;`
+    connection.query(queryString, function(err, result) {
+      if (err) throw err;
+      cb(result);
+    })
   },
-  insert: function() {
-
+  insert: function(value, cb) {
+    let queryString = `
+    INSERT INTO books (book_info)
+    VALUES (${value}, false);`
+    connection.query(queryString, function(err, result) {
+      if (err) throw err;
+      cb(result);
+    })
   },
-  update: function() {
-
-  }
+  update: function(id, boolean, cb) {
+    let queryString = `
+    UPDATE books SET finished = ${boolean}
+    WHERE id = ${parseInt(id)}`
+    connection.query(queryString, function(err, result) {
+      if (err) throw err;
+      cb(result);
+    })
+  },
+  delete: function(id, cb) {
+    let queryString = `
+    DELETE FROM books
+    WHERE id = ${parseInt(id)}`
+    connection.query(queryString, function(err, result) {
+      if (err) throw err;
+      cb(result);
+    })
+  },
 }
 
 module.exports = orm;

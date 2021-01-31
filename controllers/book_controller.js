185572f78626b29f13c06a72a2d7343445e8b43a
Jordan Kelly <profjjk@gmail.com>
2021-01-30 19:40:49 -0800

    create book obj in models

---
+++ controllers/book_controller.js
@@ -0,0 +1,7 @@
+// DEPENDENCIES
+// ================================================
+const express = require('express');
+const book = require('/../models/book.js')
+
+const router = express.Router();
+
---
+++ models/book.js
@@ -0,0 +1,28 @@
+// DEPENDENCIES
+// ================================================
+const orm = require('/../config/orm.js');
+
+const book = {
+  all: function(cb) {
+    orm.all(function(res) {
+      cb(res);
+    });
+  },
+  insert: function(value, cb) {
+    orm.insert(value, function(res) {
+      cb(res);
+    });
+  },
+  update: function(id, boolean, cb) {
+    orm.update(id, boolean, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(id, cb) {
+    orm.delete(id, function(res) {
+      cb(res);
+    });
+  },
+}
+
+module.exports = book;

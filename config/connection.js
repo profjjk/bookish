5bbeb9cdacc43f64ff813bbd81a14765ecebefa2
Jordan Kelly <profjjk@gmail.com>
2021-02-01 16:58:38 -0800

    add JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,24 @@
 // DEPENDENCIES
 // ================================================
 const mysql = require('mysql');
+let connection;
 
 
 // DATABESE CONNECTION
 // ================================================
 // Configure connection.
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "bookish_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "bookish_db"
+  });
+}
+
 
 // Make connexction.
 connection.connect(err => {
